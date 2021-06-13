e8ba1019bb79ac4daed8d241e172ea631b0a7816
Jake <jbourne2021@fau.edu>
2021-06-13 02:06:39 -0400

    new drivers

--- util.go
+++ util.go
@@ -10,10 +10,6 @@ func toMercury(m float64) float64 {
 	return m * 0.02953
 }
 
-func convert32(value float32) string {
-	return strconv.FormatFloat(float64(value), 'e', 2, 32)
-}
-
 func convert64(value float64) string {
 	return strconv.FormatFloat(float64(value), 'e', 2, 32)
 }
